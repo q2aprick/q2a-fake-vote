9c029b0e0c966aba867086e0e63563b0076340bc
q2aprick <74082290+q2aprick@users.noreply.github.com>
2020-12-10 10:06:36 +0700

    Create qa-plugin.php

---
+++ qa-plugin.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+	Plugin Name: Q2A Alpahnumeric Username
+	Plugin URI: https://github.com/q2aprick/q2a-fake-vote
+	Plugin Update Check URI: 
+	Plugin Description: Increase vote +5.
+	Plugin Version: 1.0
+	Plugin Date: 2020-12-10
+	Plugin Author: Q2A Prick
+	Plugin Author URI: https://github.com/q2aprick
+	Plugin Minimum Question2Answer Version:
+	Plugin Minimum PHP Version:
+	Plugin License: copy lefted          
+*/
+
+
+	if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+		header('Location: ../../');
+		exit;
+	}
+
+	qa_register_plugin_layer('fakevote.php', 'Fake Vote Layer');
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
